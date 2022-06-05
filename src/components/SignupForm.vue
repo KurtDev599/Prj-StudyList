f383b54ab6810b806496269e0a49308e742919a0
KurtDev599 <sw536880@naver.com>
2022-06-05 15:53:41 +0900

    Form Development

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,5 +1,19 @@
 <template>
-	<div>SignupForm</div>
+	<form>
+		<div>
+			<label for="username">id :</label>
+			<input id="username" type="text" />
+		</div>
+		<div>
+			<label for="password">password :</label>
+			<input id="password" type="text" />
+		</div>
+		<div>
+			<label for="nickname">nickname :</label>
+			<input id="nickname" type="text" />
+		</div>
+		<button type="submit">login</button>
+	</form>
 </template>
 
 <script>
